962ef1c070021420efd6c4b772ef827617f71fa7
nathan <nathan@jmwest.net>
2018-01-08 16:59:33 -0700

    inital commit

---
+++ include/mycode/mycode.h
@@ -0,0 +1,7 @@
+/***
+  Copyright 2018 J. West
+ */
+#ifndef INCLUDE_MYCODE_MYCODE_H_
+#define INCLUDE_MYCODE_MYCODE_H_
+double squareRoot(const double a);
+#endif  // INCLUDE_MYCODE_MYCODE_H_
---
+++ src/mycode.cpp
@@ -0,0 +1,14 @@
+/***
+  Copyright 2018 J. West
+ */
+#include <math.h>
+#include "mycode/mycode.h"
+double squareRoot(const double a) {
+  double b = sqrt(a);
+  if (b != b) {    // nan check
+    return -1.0;
+  } else {
+    return sqrt(a);
+  }
+}
+
