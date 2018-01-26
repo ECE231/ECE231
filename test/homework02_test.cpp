eda5a73da5c1479dffdb364906295e56dd10fadc
Jonathan West <jmwest@unm.edu>
2018-01-26 16:26:48 +0000

    updated to final test code

--- test/homework02_test.cpp
+++ test/homework02_test.cpp
@@ -3,9 +3,22 @@
  */
 #include <gtest/gtest.h>
 #include "homework02/homework02.h"
+TEST(permutations, limits) {
+  EXPECT_EQ(permutations(0, 0), 0);
+  EXPECT_EQ(permutations(-1, 0), 0);
+  EXPECT_EQ(permutations(0, -1), 0);
+  EXPECT_EQ(permutations(10, 11), 0);
+}
+  
 TEST(permutations, all) {
   EXPECT_EQ(permutations(52, 2), 2652);
 }
+TEST(combinations, limits) {
+  EXPECT_EQ(combinations(0, 0), 0);
+  EXPECT_EQ(combinations(-1, 0), 0);
+  EXPECT_EQ(combinations(0, -1), 0);
+}
+
 TEST(combinations, all) {
   EXPECT_EQ(combinations(6, 2), 15);
 }
