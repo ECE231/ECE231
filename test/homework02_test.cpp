3adc3323f07d484f6dac00ac56a07b0925bba2dc
Jonathan West <jmwest@unm.edu>
2018-01-28 00:46:26 +0000

    fixed error in test functions for k = 0w

--- test/homework02_test.cpp
+++ test/homework02_test.cpp
@@ -5,7 +5,7 @@
 #include "homework02/homework02.h"
 TEST(permutations, limits) {
   EXPECT_EQ(permutations(0, 0), 1);
-  EXPECT_EQ(permutations(10, 0), 0);
+  EXPECT_EQ(permutations(10, 0), 1);
   // k can't be greater than n so this is an invalid value
   EXPECT_EQ(permutations(10, 11), 0);
 }
@@ -17,7 +17,7 @@ TEST(permutations, all) {
 TEST(combinations, limits) {
   // factorial of 0 is 1
   EXPECT_EQ(combinations(0, 0), 1);
-  EXPECT_EQ(combinations(0, 10), 0);
+  EXPECT_EQ(combinations(0, 10), 1);
   // k can't be greater than n so this is an invalid value
   EXPECT_EQ(permutations(10, 11), 0);
 }
