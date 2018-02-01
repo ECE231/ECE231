bb81bc967cd58b15a036b2d0bb58ea4cd68cd9f0
Jonathan West <jmwest@unm.edu>
2018-02-01 23:51:56 +0000

    finished hw04 test and solution

--- test/homework04_test.cpp
+++ test/homework04_test.cpp
@@ -4,10 +4,25 @@
 #include "homework04/homework04.h"
 #include "grading/compare_files.h"
 
-TEST(histogram, all) {
-  int res = std::system("histogram good_input.txt > output.txt");
+TEST(histogram, good) {
+  int res = std::system("homework04/histogram good_input.txt > output.txt");
   EXPECT_EQ(res, 0);
   EXPECT_TRUE(compare_files("output.txt", "good_histogram_result.txt"));
+}
+
+TEST(histogram, no_file_supplied) {
+  int res = std::system("homework04/histogram");
+  EXPECT_NE(res, 0);
+}
+
+TEST(histogram, no_file) {
+  int res = std::system("homework04/histogram this_file_does_not_exist.txt");
+  EXPECT_NE(res, 0);
+}
+  
+TEST(histogram, bad_file) {
+  int res = std::system("homework04/histogram bad_input.txt");
+  EXPECT_NE(res, 0);
 }
   
 int main(int argc, char **argv) {
