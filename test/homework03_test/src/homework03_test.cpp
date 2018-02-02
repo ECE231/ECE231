c12b713c527f7171fd13d332d855ae177b788a94
Jonathan West <jmwest@unm.edu>
2018-02-02 17:50:24 +0000

    made tests into their own projects to avoid conflicts, added compare_files library.  fixed bug in homewor03 grading

--- test/homework03_test/src/homework03_test.cpp
+++ test/homework03_test/src/homework03_test.cpp
@@ -2,6 +2,7 @@
 #include <gtest/gtest.h>
 #include <fstream>
 #include "homework03/homework03.h"
+#include "compare_files/compare_files.h"
 
 TEST(encrypt, all) {
   EXPECT_STREQ(encrypt("WORKERS OF THE WORLD UNITE!", 
@@ -15,24 +16,9 @@ TEST(decrypt, all) {
     "WORKERS OF THE WORLD UNITE!"); 
 }
 
-bool compareFiles(const std::string &file1, const std::string &file2) {
-  std::ifstream f1(file1.c_str());
-  std::ifstream f2(file2.c_str());
-  while(f1.good() && f2.good()) {
-    char c1, c2;
-    f1.get(c1);
-    f2.get(c2);
-    if (c1 != c2) {
-      return false;
-    }
-  }
-  return true;
-}
-
-
 TEST(tabs_to_spaces, all) {
   tabs_to_spaces("tab_input.txt", 2, "tab_output.txt");
-  EXPECT_TRUE(compareFiles("tab_output.txt", "tab_correct_result.txt"));
+  EXPECT_TRUE(compare_files("tab_output.txt", "tab_correct_result.txt"));
 }
   
 int main(int argc, char **argv) {
