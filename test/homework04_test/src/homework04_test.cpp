3a09cdfd75c82ae2bdeaba98075fb9a9021d9c80
Jonathan West <jmwest@unm.edu>
2018-02-02 18:22:35 +0000

    homework4 passes test under new format

--- test/homework04_test/src/homework04_test.cpp
+++ test/homework04_test/src/homework04_test.cpp
@@ -2,26 +2,26 @@
 #include <gtest/gtest.h>
 #include <fstream>
 #include "homework04/homework04.h"
-#include "grading/compare_files.h"
+#include "compare_files/compare_files.h"
 
 TEST(histogram, good) {
-  int res = std::system("homework04/histogram good_input.txt > output.txt");
+  int res = std::system("../../homework04/histogram good_input.txt > output.txt");
   EXPECT_EQ(res, 0);
-  EXPECT_TRUE(compare_files("output.txt", "good_histogram_result.txt"));
+  EXPECT_TRUE(compare_files("output.txt", "correct_output.txt"));
 }
 
 TEST(histogram, no_file_supplied) {
-  int res = std::system("homework04/histogram");
+  int res = std::system("../../homework04/histogram");
   EXPECT_NE(res, 0);
 }
 
 TEST(histogram, no_file) {
-  int res = std::system("homework04/histogram this_file_does_not_exist.txt");
+  int res = std::system("../../homework04/histogram this_file_does_not_exist.txt");
   EXPECT_NE(res, 0);
 }
   
 TEST(histogram, bad_file) {
-  int res = std::system("homework04/histogram bad_input.txt");
+  int res = std::system("../../homework04/histogram bad_input.txt");
   EXPECT_NE(res, 0);
 }
   
