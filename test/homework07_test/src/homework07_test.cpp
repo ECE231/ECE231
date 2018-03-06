726bd85297992a4415eb4c99a1a38bf5b1c81b32
Jonathan West <jmwest@unm.edu>
2018-03-06 23:47:43 +0000

    Fixed grading script to allow start square to be part of the path

--- test/homework07_test/src/homework07_test.cpp
+++ test/homework07_test/src/homework07_test.cpp
@@ -7,7 +7,13 @@
 TEST(solver, good) {
   int res = std::system("../../homework07/solve_maze ShortestPathMaze.txt");
   EXPECT_EQ(res, 0);
-  EXPECT_TRUE(compare_files("output.txt", "correct_output.txt"));
+  // try with start square properly marked
+  bool good = compare_files("output.txt", "correct_output.txt");
+  // if no match with start square then try with start marked with X
+  if(!good) {
+    std::cout << "Trying with start square as part of path\n";
+    EXPECT_TRUE(compare_files("output.txt", "correct_output_with_start.txt"));
+  }
 }
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
